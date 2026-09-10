77bb2f4f31c2e6fc4b74300faee1cf3273644328
mdarzand0315-sys <m.darzand0315@gmail.com>
2026-09-11 01:51:44 +0330

    Update route.ts

--- app/api/matches/route.ts
+++ app/api/matches/route.ts
@@ -1,47 +1,29 @@
-import { prisma } from "../../../lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  try {
-    const matches = await prisma.match.findMany({
-      orderBy: {
-        date: "asc",
-      },
-      include: {
-        league: true,
-      },
-    });
+  const matches = [
+    {
+      id: 1,
+      league: "لیگ برتر استان بوشهر",
+      week: 1,
+      homeTeam: "شاهین بوشهر",
+      awayTeam: "پارس جنوبی جم",
+      score: "0 - 0",
+      date: "1405/06/20"
+    }
+  ];
 
-    return NextResponse.json(matches);
-  } catch (error) {
-    return NextResponse.json(
-      { error: "خطا در دریافت مسابقات" },
-      { status: 500 }
-    );
-  }
+  return NextResponse.json(matches);
 }
 
+
 export async function POST(request: Request) {
-  try {
-    const body = await request.json();
 
-    const match = await prisma.match.create({
-      data: {
-        week: body.week,
-        homeTeam: body.homeTeam,
-        awayTeam: body.awayTeam,
-        homeScore: body.homeScore ?? null,
-        awayScore: body.awayScore ?? null,
-        date: new Date(body.date),
-        leagueId: body.leagueId,
-      },
-    });
+  const body = await request.json();
 
-    return NextResponse.json(match);
-  } catch (error) {
-    return NextResponse.json(
-      { error: "خطا در ثبت مسابقه" },
-      { status: 500 }
-    );
-  }
+  return NextResponse.json({
+    message: "مسابقه ثبت شد",
+    data: body
+  });
+
 }
