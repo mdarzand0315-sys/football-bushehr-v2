4455ea597a8bca0e00d42ae217607321f7cc3181
mdarzand0315-sys <m.darzand0315@gmail.com>
2026-09-11 11:24:15 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 
-import HeroSection from "@/components/HeroSection";
-import StatsSection from "@/components/StatsSection";
+import HeroSection from "../components/HeroSection";
+import StatsSection from "../components/StatsSection";
 
 export default function Home(){
 return (
