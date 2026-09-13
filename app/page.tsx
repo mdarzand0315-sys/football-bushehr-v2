e4f7b6fab08c3f2b25731b7ed5d5a739e4128d56
mdarzand0315-sys <m.darzand0315@gmail.com>
2026-09-14 01:14:41 +0330

    Expand homepage layout with news and matches sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-
 import HeroSection from "../components/HeroSection";
 import StatsSection from "../components/StatsSection";
+import HomeSections from "../components/HomeSections";
 
 export default function Home(){
 return (
 <>
 <HeroSection/>
 <StatsSection/>
+<HomeSections/>
 </>
 )
 }
